3abe13400e411e62098d24ac7868bbb6f50609a8
Emelie572 <wahlestedt.emelie@gmail.com>
2024-12-09 11:35:39 +0100

    Contactdetails klass skapad med string + grunden till metoder

---
+++ src/ContactDetails.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ContactDetails {
+
+    Scanner scanner;
+
+    private String contactDetails = """
+            === Kontaktuppgifter ===
+            Stugan
+            Företagsadress:
+            Telefontider: 8-20
+            Tele-nummer: 0700000000 
+            Email: stugan@hotmail.com
+            """;
+
+
+    public ContactDetails(Scanner scanner) {
+        this.scanner = scanner;
+
+    }
+
+    public void printMenu() {
+    }
+
+    public void printExitInstructions() {
+    }
+
+    public void exitClass() {
+    }
+
+}
