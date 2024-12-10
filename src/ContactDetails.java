bc7bf14514742e8a42c92aac03f86b055ab18d35
Emelie572 <wahlestedt.emelie@gmail.com>
2024-12-10 11:57:11 +0100

    uppdaterat runmenu() + printmenu() + småfix

--- src/ContactDetails.java
+++ src/ContactDetails.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 
-public class ContactDetails implements MenuScreen{
+public class ContactDetails implements MenuScreen {
 
     Scanner scanner;
 
     private String contactDetails = """
             === Kontaktuppgifter ===
             Stugan
-            Företagsadress:
+            Företagsadress: 
+            Hemsida: https://stugan-myrsjo.se
             Telefontider: 8-20
             Tele-nummer: 0700000000 
             Email: stugan@hotmail.com
@@ -16,17 +17,60 @@ public class ContactDetails implements MenuScreen{
 
     public ContactDetails(Scanner scanner) {
         this.scanner = scanner;
-
+        System.out.println(contactDetails);
     }
 
     @Override
     public void printMenu() {
+
+        String menuTitle = String.format(
+                "För önskat val --> skriv motsvarande siffra %n" +
+                        "(2) Tillbaka till huvudmenyn");
+
+        System.out.println(menuTitle);
+        runMenu();
     }
 
+    public void runMenu() {
+            int userInput = checkUserInput();
+            if (userInput == 2) {
+                //tillbaka till huvudmenyn
+            }
+        }
+
+
+    public int checkUserInput() {
+        int userChoiceInput = 0;
+
+        try {
+            userChoiceInput = Integer.parseInt(scanner.nextLine().trim());
+
+            if (userChoiceInput != 2) {
+                System.out.println("Felaktig inmatning --> Ange en giltig siffra");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Felaktig inmatning --> Endast siffror kan anges vid val, försök igen! ");
+
+        }
+        return userChoiceInput;
+    }
+
+    //blir överflödig
+    @Override
     public void printExitInstructions() {
-    }
+        boolean validInput = false;
 
-    public void exitClass() {
-    }
+        while (!validInput) {
+            System.out.println("Skriv 'Exit' eller 'E' för att återgå till huvudmenyn:");
+            String exitInput = scanner.nextLine();
 
+            if (exitInput.equalsIgnoreCase("Exit") || exitInput.equalsIgnoreCase("E")) {
+                //vad istället för exitclass?
+                validInput = true;
+            } else {
+                System.out.println("Felaktig inmatning, försök igen.");
+            }
+
+        }
+    }
 }
