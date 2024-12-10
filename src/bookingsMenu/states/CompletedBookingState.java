a71747d76db1b4411df1e245fd596abb9328ca8d
Vasahlin <Vasahlin@gmail.com>
2024-12-10 19:37:41 +0100

    Extracted printing instructions in CompletedBookingState

--- src/bookingsMenu/states/CompletedBookingState.java
+++ src/bookingsMenu/states/CompletedBookingState.java
@@ -6,18 +6,24 @@ import utility.Utility;
 public class CompletedBookingState implements BookingState {
     @Override
     public void handle(BookingsMenu context) {
-        String yellow = "\u001B[33m";
-        String resetColor = "\u001B[0m";
-        Utility.clearScreen();
         addBookings(context);
-        System.out.println(yellow + "Du har bokat:" + resetColor);
-        context.getBooking().showBooking();
-        System.out.println();
+        printBooking(context);
+        context.getBooking().setPending(false);
         context.setCurrentState(null);
     }
+
     private void addBookings(BookingsMenu context) {
         for (String week : context.getPendingBookings()) {
             context.getBooking().addWeek(week);
         }
+    }
+
+    private void printBooking(BookingsMenu context) {
+        String yellow = "\u001B[33m";
+        String resetColor = "\u001B[0m";
+        Utility.clearScreen();
+        System.out.println(yellow + "Du har bokat:" + resetColor);
+        context.getBooking().showBooking();
+        System.out.println();
     }
 }
