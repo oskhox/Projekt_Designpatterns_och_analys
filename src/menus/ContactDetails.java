8cbc2f4487a36c1d88f7b85b62255af7c539cf3c
Oskar Høxbroe <oskar.hoxbroe@gmail.com>
2024-12-13 10:15:37 +0100

    småfix

--- src/menus/ContactDetails.java
+++ src/menus/ContactDetails.java
@@ -8,13 +8,13 @@ public class ContactDetails implements MenuScreen {
 
     Scanner scanner;
 
-    private String contactDetails = """
+    private final String contactDetails = """
             === Kontaktuppgifter ===
             Stugan
-            Företagsadress: 
+            Företagsadress: Rödmyravägen 5, Järvsö
             Hemsida: https://stugan-myrsjo.se
             Telefontider: 8-20
-            Tele-nummer: 0700000000 
+            Tele-nummer: 0700000000
             Email: stugan@hotmail.com
             """;
 
